784d5da547ef797756f587bcccf44af30a816286
Anastasiya Solovyova <rabtsavaas@gmail.com>
2024-04-24 16:20:07 +0300

    typewriter

--- src/assets/layout/sections/main/Main.tsx
+++ src/assets/layout/sections/main/Main.tsx
@@ -3,7 +3,8 @@ import { FlexWrapper } from '../../../../components/FlexWrapper';
 import photo from '../../../images/photo.jpg';
 import {Container} from "../../../../components/Container";
 import {S} from './Main_Styles'
-
+import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 
 export const Main:React.FC = () => {
@@ -14,12 +15,23 @@ export const Main:React.FC = () => {
                 <div>
                     <S.SmallText>Hi There</S.SmallText>
                     <S.Name>I am <span>Anastasiya Solovyova</span></S.Name>
-                    <S.MainTitle>A Web Developer.</S.MainTitle>
+                    <S.MainTitle>
+                        <p>A Web Developer.</p>
+                        <Typewriter
+                            options={{
+                                strings: ['A Web Developer.'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 150
+                            }}
+                        />
+                    </S.MainTitle>
                 </div>
-                <S.PhotoWrapper>
-                <S.Photo src={photo} alt=""/>
-                </S.PhotoWrapper>
-
+                <Tilt>
+                    <S.PhotoWrapper>
+                        <S.Photo src={photo} alt=""/>
+                    </S.PhotoWrapper>
+                </Tilt>
             </FlexWrapper>
             </Container>
         </S.Main>
